42dbfc8733c12cf3e06912f1645cde408abd621b
Emmanuel Obeng Twene <delinuxist@gmail.com>
2024-08-24 23:28:28 +0000

    setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack(config) {
+    config.module.rules.push(
+      // Handle SVGs as React components
+      {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        use: ["@svgr/webpack"],
+      },
+      // Handle SVGs as URLs (when imported with ?url)
+      {
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
+        type: "asset/resource",
+      },
+    );
+    return config;
+  },
+};
 
 export default nextConfig;
