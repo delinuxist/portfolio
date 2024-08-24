700879bb5b0d1961e1fbea41b595013ef5e1607f
Emmanuel Obeng Twene <delinuxist@gmail.com>
2024-08-24 23:29:43 +0000

    navbar

---
+++ src/components/layouts/navbar.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className='flex justify-center items-center fixed top-3'>
+      <nav className=' flex gap-1 border rounded-full p-0.5 bg-white/10 backdrop-blur'>
+        <a href='#' className='nav-item'>
+          Home
+        </a>
+        <a href='#' className='nav-item'>
+          Projects
+        </a>
+        <a href='#' className='nav-item'>
+          About
+        </a>
+        <a
+          href='#'
+          className='nav-item bg-card-foreground !text-card hover:bg-card-foreground/70 hover:!text-card'
+        >
+          Contact
+        </a>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
